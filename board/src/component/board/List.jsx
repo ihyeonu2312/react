5f6e1bb55767934fe42621eeebd33b7e4b8f965a
ihyeonu <dlgusdn2312@gmail.com>
2025-01-17 16:14:57 +0900

    좋아요 토글

--- board/src/component/board/List.jsx
+++ board/src/component/board/List.jsx
@@ -24,7 +24,7 @@ const List=()=>{
     <div>
       <button onClick={()=>navigate('/write')}>글쓰기</button>
       <ul>
-        {data && data.map(b => <li key={b.bno}>{b.title}</li>)}
+        {data && data.map(b => <li key={b.num}><Link to={`/notes/${b.num}`}>{b.title}</Link> <span>좋아요 {b.likesCnt}</span> <span> {b.attachCnt > 0 && 'm'}</span></li>)}
       </ul>
     </div>
   )
--- board/src/component/board/View.jsx
+++ board/src/component/board/View.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAxios from '../../hooks/useAxios';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../hooks/AuthContext';
 
 const View = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+
+  const param = useParams();
+  const num = param.num;
+  const {loading, error, req} = useAxios();
+  const nav = useNavigate();
+  const {email} = useAuth();
+  const [note, setNote] = useState({});
+  const [myLike, setMyLike] = useState({});
+
+
+  // effect >> api 호출
+  const o = {email, num};
+  useEffect(() => {
+
+    (async() => {
+      setNote(await req('get', `notes/${num}`));
+      const queryString = new URLSearchParams(o).toString();
+      setMyLike(await req('get', `likes?${queryString}`));
+    })();
+  }, [num, req, email]);
+
+  if(error) {
+    return <div><h1>에러발생</h1></div>
+  }
+
+
+ if(loading) {
+   return <div><h1>로딩중</h1></div>
+}
   // 삭제 처리
 const handleDelete = e => {
   e.preventDefault();
@@ -16,23 +43,31 @@ const handleDelete = e => {
   req('delete', `notes/${num}`);
   nav('/notes');
 };
-return data && (
+
+// 좋아요 토글
+const handleLikesToggle = async e => {
+  e.preventDefault();
+  await req('get', `likes`, {email, num}) ;
+  setMyLike
+}
+return note && (
   <div>
     <h1>View</h1>
     <p>{param.num} 번 게시글</p>
-    <p>{data.title}</p>
-    <p>{data.content}</p>
-    <p>{data.writer}</p>
-    <p>{data.regDate}</p>
-    <p>{data.modDate}</p>
-    
+    <p>{note.title}</p>
+    <p>{note.content}</p>
+    <p>{note.writer}</p>
+    <p>{note.regDate}</p>
+    <p>{note.modDate}</p>
+    <p><button>좋아요<span style={{color: 'red'}}>{myLike ? '♥' : '♡'}</span> {note.likesCnt}</button></p>
+
     <div>
-      <h3>attachs : {data.attachDtos.length}</h3>
+      <h3>attachs : {note.attachDtos && note.attachDtos.length}</h3>
       <ul>
-        {data.attachDtos.map(a => <li key={a.uuid}><Link to={a.uuid}>{a.origin}</Link></li>)}
+        {note.attachDtos && note.attachDtos.map(a => <li key={a.uuid}><Link to={a.url}>{a.origin}</Link></li>)}
       </ul>
     </div>
-    <Link to={`/notes/modify/${data.num}`}><button>수정</button></Link>
+    <Link to={`/notes/modify/${note.num}`}><button>수정</button></Link>
     <button onClick={handleDelete}>삭제</button>
   </div>
 )
